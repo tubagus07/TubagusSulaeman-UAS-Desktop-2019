69e7bb0e8d4d7cf0fb4113db11854dd7636df2db
tubagus sulaeman <sidikibnu301@gmail.com>
2019-05-22 10:14:25 +0700

    Commit pertama

---
+++ src/tubagussulaeman/uas/desktop/pkg2019/TubagusSulaemanUASDesktop2019.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tubagussulaeman.uas.desktop.pkg2019;
+
+/**
+ *
+ * @author PeTIK
+ */
+public class TubagusSulaemanUASDesktop2019 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
